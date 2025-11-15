fd53e4ba1bb90f579c908a2ac916c9b1797bf5ac
sandrrovna <sandrrovna@gmail.com>
2025-11-16 02:17:09 +0300

    Update .java

--- LB1/.java
+++ LB1/.java
@@ -1,5 +1,7 @@
+//Создание массива на JAVA
 string[] lst = new string[] {'Пушкин', 'Есенин', 'Толстой'};
 
+//Создание стека на JAVA
 MyStack stack = new MyStack();
 stack.push('Пушкин');
 stack.push('Есенин');
